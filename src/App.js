d99bbac17334d64a68ab0c9483aaa0fca0ae7994
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-02-23 22:34:04 +0700

    a

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './Template/NavBar'
+import Record from './Record/Record'
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-    </div>
+    <>
+      <div className="App">
+        <NavBar/>
+      </div>
+      <div className="container">
+        <Record/>
+      </div>
+    </>
   );
 }
 
---
+++ src/Record/Record.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import {ReactMic} from 'react-mic'
+import {Button} from 'react-bootstrap'
+export default class Record extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            record: false
+        }
+    }
+
+    startRecording = () => {
+        this.setState({
+            record: true
+        })
+    }
+
+    stopRecording = () => {
+        this.setState({
+            record: false
+        })
+    }
+
+    onData(recordedBlob) {
+        console.log('chunk of real time data is: ', recordedBlob)
+    }
+
+    onStop(recordedBlob) {
+        console.log('recordedBlob is: ',recordedBlob)
+    }
+
+    render() {
+        return (
+            <div>
+                <ReactMic
+                    record={this.state.record}
+                    className="sound-wave col"
+                    // onData={this.onData}  
+                    onStop={this.onStop}
+                    strokeColor="#000000"
+                    backgroundColor="#FF4081"
+                />
+                <div className="row justify-content-center mt-2">
+                    <Button onClick={this.startRecording} variant="primary" className="text-capitalize mx-1 col-2">
+                        Start record
+                    </Button>
+                    <Button onClick={this.stopRecording} variant="danger" className="text-capitalize mx-1 col-2">
+                        Stop record
+                    </Button>
+                </div>
+            </div>
+        )
+    }
+}
