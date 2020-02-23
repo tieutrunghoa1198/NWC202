768be02e3c1c0e08871d5ee5c99e694ca486ff7b
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-02-23 17:55:07 +0700

    STT to TTS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/jquery/dist/jquery.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
