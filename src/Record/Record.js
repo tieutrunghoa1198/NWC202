5cdfd3b83ff2e992d6d1b00f7be1b52787a1f2ed
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-02-25 21:57:34 +0700

    initial commit

--- src/Record/Record.js
+++ src/Record/Record.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import {ReactMic} from 'react-mic'
 import {Button} from 'react-bootstrap'
-// import axios from 'axios'
+import axios from 'axios'
 export default class Record extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            url: '',
+            responseText: '',
             record: false
         }
         this.onStop = this.onStop.bind(this)
     }
 
-    sendRequest = () => {
-        // url
+    setText = (responseText) => {
+        this.setState({
+            responseText: responseText
+        })
     }
 
     startRecording = () => {
@@ -22,24 +24,41 @@ export default class Record extends Component {
             record: true
         })
     }
-
+    
     stopRecording = () => {
         this.setState({
             record: false
         })
     }
-
-    onData(recordedBlob) {
-        console.log('chunk of real time data is: ', recordedBlob)
+    
+    // blobToFile = (blob, fileName) => {
+    //     blob.lastModifiedDate = new Date()
+    //     blob.name = fileName
+    //     return blob
+    // }
+    
+    sendRequest = (blob) => {
+        let responseText
+        let url = 'https://api.fpt.ai/hmi/asr/general'
+        let headers = {
+            'api-key': 'VR5x7QPp5R6iQxniZ9S6o2JIXfftBRaf'
+        }
+        axios({
+            url: url,
+            method: "POST",
+            data: blob,
+            headers
+        }).then(response => {
+            console.log(response.data.hypotheses[0].utterance);
+            responseText = response.data.hypotheses[0].utterance
+        }).catch(err => {
+            console.log(err);
+        })
+        this.setText(responseText)
     }
 
     onStop(recordedBlob) {
-        // var url = recordedBlob.blobURL
-        console.log('recordedBlob is: ',recordedBlob.blobURL)
-        this.setState({
-            url: recordedBlob.blobURL
-        })
-        console.log('State URL: ',this.state.url)
+        this.sendRequest(recordedBlob.blob)
     }
 
     render() {
@@ -50,6 +69,7 @@ export default class Record extends Component {
                     className="sound-wave w-100"
                     // onData={this.onData}  
                     onStop={this.onStop}
+                    mimeType="audio/mp3"
                     strokeColor="#000000"
                     backgroundColor="#FF4081"
                 />
@@ -61,10 +81,6 @@ export default class Record extends Component {
                     <Button onClick={this.stopRecording} variant="danger" 
                     className="text-capitalize my-1 mx-1 col-6 col-sm-3 col-xl-2">
                         Stop record
-                    </Button>
-                    <Button onClick={this.stopRecording} variant="danger" 
-                    className="text-capitalize my-1 mx-1 col-6 col-sm-3 col-xl-2">
-                        Send request
                     </Button>
                 </div>
             </>
