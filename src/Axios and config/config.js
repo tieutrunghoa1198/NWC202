c02d0dffbd70efdbd4c4ebbf530c1af460b8eaf8
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-03-12 17:53:21 +0700

    update api

--- src/Axios and config/config.js
+++ src/Axios and config/config.js
@@ -1,14 +1,15 @@
+const KEY = process.env.REACT_APP_STATE === 'localhost' ? 'petQoJTJmXHa6v5sY9IwdJmOzAvyaYqy' : process.env.APIKEY
 export default {
     STT: {
         rootPath: 'https://api.fpt.ai/hmi/asr/general',
         headers: {
-            'api-key': '8EIqiB5Ih5sao0W2eiNoqq9F51V7DPeW'
+            'api-key': KEY
         }
     },
     TTS: {
         rootPath: 'https://api.fpt.ai/hmi/tts/v5',
         headers: {
-            'api-key': '8EIqiB5Ih5sao0W2eiNoqq9F51V7DPeW',
+            'api-key': KEY,
             'voice': 'banmai',
             'Content-Type': 'application/x-www-form-urlencoded'
         }
