b37cb791ca9adf1a0e94b844482b56631754badc
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-03-22 00:02:02 +0700

    comit

--- src/Components/Record/StopButton.js
+++ src/Components/Record/StopButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 export default function StopButton(props) {
     return (
         <div
-        //commit
+        //delete commit
             className="btn btn-th btn-danger my-1 mx-1 col-6 col-sm-2 col-xl-2"
             onClick={props.stopRecording}>
             Stop
