720379b2a94ba06a45b27cbb8f3c0beff8c1c220
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-03-30 08:39:46 +0700

    commit

--- src/Components/Template/NavBar.js
+++ src/Components/Template/NavBar.js
@@ -38,16 +38,10 @@ export default class NavBar extends Component {
                             </li>
                         </ul>
                         <div className="dropdown-divider"></div>
-                        <form action="/login" className="form-inline my-2 my-lg-0">
+                        <form action="http://localhost:8080/login" className="form-inline my-2 my-lg-0">
                             <button className="btn btn-outline-success mr-2 my-2 my-sm-0" type="submit">Login</button>
-                            {/* <span>/</span> */}
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Sign up</button>
                         </form>
-                        {/* <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" 
-                        placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form> */}
                     </div>
                 </div>
             </nav>
